80f3895aa659e9dc9538dce3bce84bf4ad158066
MuddassirHymeed <mhk.csit@gmail.com>
2024-07-29 15:41:43 +0500

    modify nav-bar hiding fucntion list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-// Counter Machine
+// Scroll UP//
 
+function scrollTopBack() {
+
+    let scrolltopButton = document.querySelector("#scrollUp");
+
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if (scroll >= 250) {
+            scrolltopButton.classList.add("scrollActive");
+        } else {
+            scrolltopButton.classList.remove("scrollActive")
+        }
+
+    }
+}
+scrollTopBack();
+
+
+// Nav hide //
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click" ,function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
+
+// Counter Machine
 let dayitem = document.querySelector("#days");
 let hoursitem = document.querySelector("#hours");
 let minitem = document.querySelector("#min");
@@ -11,10 +41,10 @@ let countDown = () => {
     let myDate = futureDate - currentDate;
 
 
-    let days = Math.floor(myDate  / 1000 / 60 / 60 / 24);
-    let hours = Math.floor(myDate  / 1000 / 60 / 60) % 24;
-    let min = Math.floor(myDate  / 1000 / 60)  % 60;
-    let sec = Math.floor(myDate  / 1000 ) % 60;
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
 
 
     dayitem.innerHTML = days;
@@ -24,22 +54,8 @@ let countDown = () => {
 }
 
 countDown();
-setInterval(countDown,1000);
+setInterval(countDown, 1000);
 
 
-// Scroll UP//
 
-function scrollTopBack(){
-    let scrolltopButton = document.querySelector("#scrollUp");
-    window.onscroll = function () {
-        var scroll = document.documentElement.scrollTop;
-        if(scroll >= 250){
-            scrolltopButton.classList.add("scrollActive");
-        }else{
-            scrolltopButton.classList.remove("scrollActive")
-        }
 
-    }
-}
-
-scrollTopBack();

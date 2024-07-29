5173b5ffc9cdfdd2c51ff8ea3a9ede5c08380141
MuddassirHymeed <mhk.csit@gmail.com>
2024-07-29 15:28:51 +0500

    add testimonal and contact page and modify scroll bar

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Counter Machine
+
+let dayitem = document.querySelector("#days");
+let hoursitem = document.querySelector("#hours");
+let minitem = document.querySelector("#min");
+let secitem = document.querySelector("#sec");
+
+let countDown = () => {
+    let futureDate = new Date("3 Augest 2024");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+
+
+    let days = Math.floor(myDate  / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate  / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate  / 1000 / 60)  % 60;
+    let sec = Math.floor(myDate  / 1000 ) % 60;
+
+
+    dayitem.innerHTML = days;
+    hoursitem.innerHTML = hours;
+    minitem.innerHTML = min;
+    secitem.innerHTML = sec;
+}
+
+countDown();
+setInterval(countDown,1000);
+
+
+// Scroll UP//
+
+function scrollTopBack(){
+    let scrolltopButton = document.querySelector("#scrollUp");
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrolltopButton.classList.add("scrollActive");
+        }else{
+            scrolltopButton.classList.remove("scrollActive")
+        }
+
+    }
+}
+
+scrollTopBack();
